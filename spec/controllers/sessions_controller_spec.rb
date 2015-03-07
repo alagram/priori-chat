e6538dcd1e25f19e89ffccfe51b35e743c50d1ac
Albert Agram <albert.agram@gmail.com>
2015-03-07 13:51:57 +0000

    sessiosn controller happy path test cases

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe SessionsController do
+  describe "POST create" do
+    context "with valid inputs" do
+
+      it "sets username into the session"
+      it "sets dialect into the session"
+      it "redirects to home path"
+      it "sets flash success message"
+    end
+
+    context "with invalid inputs"
+  end
+end

6f166879f5241368e4cd42026b377039adaf7360
Albert Agram <albert.agram@gmail.com>
2015-03-07 13:59:32 +0000

    sessions controller happy path test cases done

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,11 +3,20 @@ require 'rails_helper'
 describe SessionsController do
   describe "POST create" do
     context "with valid inputs" do
+      before { post :create, username: "albert", dialect: "yoda" }
 
-      it "sets username into the session"
-      it "sets dialect into the session"
-      it "redirects to home path"
-      it "sets flash success message"
+      it "sets username into the session" do
+        expect(session[:username]).to eq("albert")
+      end
+      it "sets dialect into the session" do
+        expect(session[:dialect]).to eq("yoda")
+      end
+      it "redirects to home path" do
+        expect(response).to redirect_to home_path
+      end
+      it "sets flash success message" do
+        expect(flash[:success]).to eq("Signed in.")
+      end
     end
 
     context "with invalid inputs"

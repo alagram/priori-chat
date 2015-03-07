ff9e89deca5be7ab906fa725695bde211b0590df
Albert Agram <albert.agram@gmail.com>
2015-03-07 22:19:44 +0000

    messages#index and messages#create actions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,5 +2,11 @@ class MessagesController < ApplicationController
   before_action :require_user
 
   def index
+    @message = Message.new
+    @messages = Message.all
+  end
+
+  def create
+    @message = Message.create(params.require(:message).permit(:body).merge!(author: current_user))
   end
 end

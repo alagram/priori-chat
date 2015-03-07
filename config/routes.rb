905fed688c21ff7261385230822421cc2786679a
Albert Agram <albert.agram@gmail.com>
2015-03-07 22:19:19 +0000

    messages create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#front'
   resources :sessions, only: :create
+  resources :messages, only: :create
   get 'sign_out', to: 'sessions#destroy'
   get 'home', to: 'messages#index'
 end

6f3e94328f7cdf8aa825a828ef854e996810938e
Albert Agram <albert.agram@gmail.com>
2015-03-08 22:34:10 +0000

    helper method to display icons based on dialect

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
     uri = URI.parse("http://localhost:9292/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
+
+  def class_for(dialect)
+    if dialect == "Pirate"
+      'class="fa fa-flag"'
+    elsif dialect == "Yoda"
+      'class="fa fa-mars"'
+    else
+      'class="fa fa-venus"'
+    end
+  end
 end

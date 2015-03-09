0ef8244363b77ffcb5e26fb35d9c9048d2097650
Albert Agram <albert.agram@gmail.com>
2015-03-09 13:03:59 +0000

    messages controller index action specs

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe MessagesController do
+  describe "GET index" do
+
+    context "authenticated users" do
+
+      let(:message) { Fabricate(:message, body: "Hello world", dialect: "Pirate") }
+      let(:online_user) { Fabricate(:online_user, username: "joe") }
+      before { session[:username] = "joe" }
+
+      it "sets the @message instance variable" do
+        get :index
+        expect(assigns(:message)).to be_instance_of(Message)
+      end
+
+      it "sets the @messages instance variable" do
+        get :index
+        expect(assigns(:message)).to be_instance_of(Message)
+      end
+
+      it "sets the @online_users instance vairable" do
+        get :index
+        expect(assigns(:online_users)).to eq([online_user])
+      end
+    end
+  end
+end

3647d0de775582793bf974b1cfb686ea3af9dd17
Albert Agram <albert.agram@gmail.com>
2015-03-10 19:41:19 +0000

    minor refactor of DegreaveService

--- app/models/degreave_service.rb
+++ app/models/degreave_service.rb
@@ -13,8 +13,14 @@ class DegreaveService
     uri = URI("http://www.degraeve.com/cgi-bin/babel.cgi?d=#{dialect}&url=&w=#{encoded_message}")
     response = strip_tags(Net::HTTP.get(uri))
     upcased_dialect = dialect.upcase
-    message == "valley" ? response.gsub!("Crapola Translator\n\n\nuhoh, no header.\nTranslator : Your Words in #{upcased_dialect}\n", "")
-                     .gsub!("uhoh, no footer.", "") : response.gsub!("Crapola Translator\n\n\nuhoh, no header.\nTranslator : Your Words in #{upcased_dialect}\n", "")
+    format(response, upcased_dialect)
+  end
+
+  private
+
+  def format(a_response, altered_dialect)
+    message == "valley" ? a_response.gsub!("Crapola Translator\n\n\nuhoh, no header.\nTranslator : Your Words in #{altered_dialect}\n", "")
+                     .gsub!("uhoh, no footer.", "") : a_response.gsub!("Crapola Translator\n\n\nuhoh, no header.\nTranslator : Your Words in #{altered_dialect}\n", "")
                      .gsub!("uhoh, no footer.", "").strip
   end
 end
